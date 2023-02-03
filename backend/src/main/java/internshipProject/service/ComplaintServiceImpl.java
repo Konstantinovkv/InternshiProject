3bfb825de7ffaa757d9e8e36d71e1eda1168fb21
Konstantin Konstantinov <konstantinovkv@gmail.com>
2023-02-03 19:34:48 +0200

    Add email

--- backend/src/main/java/internshipProject/service/ComplaintServiceImpl.java
+++ backend/src/main/java/internshipProject/service/ComplaintServiceImpl.java
@@ -3,12 +3,22 @@ package internshipProject.service;
 import internshipProject.dao.Entity.Complaint;
 import internshipProject.dao.Entity.User;
 import internshipProject.dao.repository.UserRepository;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import internshipProject.dao.repository.ComplaintRepository;
 import internshipProject.dto.ComplaintResponse;
 
+import javax.mail.MessagingException;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
 
 @Service
 public class ComplaintServiceImpl implements ComplaintService {
@@ -17,6 +27,9 @@ public class ComplaintServiceImpl implements ComplaintService {
     @Autowired
     private UserRepository userRepository;
 
+    @Value("${spring.mail.username}") private String username;
+    @Value("${spring.mail.password}") private String password;
+
     @Autowired
     private ComplaintRepository complaintRepository;
 
@@ -24,12 +37,13 @@ public class ComplaintServiceImpl implements ComplaintService {
         final Complaint complaint = complaintRepository.findById(complaintId);
         if (complaint == null) {
             return null;
-        }
-        else return complaint;
+        } else return complaint;
     }
 
+    @SneakyThrows
     public void registerComplaint(ComplaintResponse complaint, User user) {
         complaintRepository.save(new Complaint(new java.sql.Date(System.currentTimeMillis()), complaint.getText(), complaint.getTopic(), user));
+        sendEmail(user);
     }
 
     @Override
@@ -39,6 +53,32 @@ public class ComplaintServiceImpl implements ComplaintService {
 
     public void deleteComplaint(long id) {
         complaintRepository.deleteById(id);
+    }
+
+    private void sendEmail(User user) throws MessagingException {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("complaints@gmail.com", false));
+
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getEmail()));
+        msg.setSubject("Sorry to hear that");
+        msg.setContent("We are sorry to hear about your problem " + user.getUsername() + "! " +
+                "\nDrink some water, maybe it will help", "text/html");
+        msg.setSentDate(new Date());
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent("", "text/html");
+        Transport.send(msg);
     }
 
 }
